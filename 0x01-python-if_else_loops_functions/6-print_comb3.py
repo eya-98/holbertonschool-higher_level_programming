0258bbcc62a4f82b87b052f785f0fed7080b848f
eya-98 <eya.nani@sesame.com.tn>
2020-12-08 17:47:28 +0000

    print comb

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-for c in range (0, 10):
+for c in range(0, 8):
     for d in range(c + 1, 10):
-        if c < 8 and d < 9:
-            print("{:d}{:d},".format(c, d), end=" ")
-        elif c == 8 and d == 9:
-            print("{:d}{:d}".format(c, d))
+        print("{:d}{:d},".format(c, d), end=" ")
+print("89")
