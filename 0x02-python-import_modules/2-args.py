22b6da7a2569fc0457cbbb7adcf1686e99727fc2
eya-98 <eya.nani@sesame.com.tn>
2020-12-10 14:39:56 +0000

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,9 +5,9 @@ if __name__ == "__main__":
     if len(argv) - 1 == 0:
         print("arguments.")
     elif len(argv) - 1 == 1:
-        print("argument: ")
+        print("argument:")
     else:
-        print("arguments: ")
+        print("arguments:")
     for i in range(1, len(argv)):
         print("{} : {}".format(i, argv[i]))
     
