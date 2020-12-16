4ffeb9708c0273cbbcfbecd2167b5a9b60dccdd5
eya-98 <eya.nani@sesame.com.tn>
2020-12-16 20:28:38 +0000

    add tuple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,5 +3,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
     tuple_a += (0, 0)
     tuple_b += (0, 0)
     a = tuple_a[0] + tuple_b[0]
-    b = tuple_b[1] + tuple_b[1]
+    b = tuple_a[1] + tuple_b[1]
     return (a , b)
