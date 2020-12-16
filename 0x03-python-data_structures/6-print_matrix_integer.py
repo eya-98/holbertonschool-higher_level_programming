5ad49117628dc8891a0d816e9be2f0e8fcd88076
eya-98 <eya.nani@sesame.com.tn>
2020-12-16 14:02:45 +0000

    print matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,6 +4,6 @@ def print_matrix_integer(matrix=[[]]):
         for j in range(len(matrix[i])):
             if j < len(matrix[i]) - 1:
                 print("{:d}".format(matrix[i][j]), end=" ")
-        else:
+            else:
                 print("{:d}".format(matrix[i][j]), end="")
         print("")
