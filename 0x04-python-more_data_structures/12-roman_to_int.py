a674117b2c32a58aed031d8e1171d14a15023d37
eya-98 <eya.nani@sesame.com.tn>
2020-12-17 21:23:08 +0000

    convert roman to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,12 +3,13 @@ def roman_to_int(roman_string):
     if (roman_string is None) or type(roman_string) != str:
         return 0
     else:
-        roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        r = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
+             'D': 500, 'M': 1000}
         sum = 0
         for i in range(len(roman_string)):
             if (i != (len(roman_string) - 1) and
-                roman[roman_string[i]] < roman[roman_string[i + 1]]):
-                sum -= roman[roman_string[i]]
+                    r[roman_string[i]] < r[roman_string[i + 1]]):
+                sum -= r[roman_string[i]]
             else:
-                sum += roman[roman_string[i]]
+                sum += r[roman_string[i]]
         return sum
