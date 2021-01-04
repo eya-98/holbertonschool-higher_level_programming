303b4f043b91c3caf515e243c2d0b0d4447dd75a
eya-98 <eya.nani@sesame.com.tn>
2021-01-04 15:47:00 +0000

    print x integers

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    j = 0
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            j += 1
+        except (TypeError, ValueError):
+            pass
+    print("")
+    return j
+        
