24868293598d423f6669aa81175b00fc82af7c88
eya-98 <eya.nani@sesame.com.tn>
2021-01-04 11:49:14 +0000

    print x number

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     j = 0
+    k = 0
     for k in my_list:
-        print("{}".format(k), end="")
-        j += 1
-        x -= 1
-        if x == 0:
-            break
+            try:
+                print("{}".format(k), end="")
+                j += 1
+                x -= 1
+                if x == 0:
+                    break
+            except:
+                pass
     return j
