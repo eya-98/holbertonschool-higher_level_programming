ee0332fddc8672347ecec7c53d2fb31866cfcb3b
eya-98 <eya.nani@sesame.com.tn>
2021-01-04 11:51:12 +0000

    print x number

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,6 +8,7 @@ def safe_print_list(my_list=[], x=0):
                 j += 1
                 x -= 1
                 if x == 0:
+                    print("\n")
                     break
             except:
                 pass
