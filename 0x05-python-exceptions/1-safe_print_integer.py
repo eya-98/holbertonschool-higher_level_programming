d74d7925c30e6b2c529275513ee656531a4e9ba9
eya-98 <eya.nani@sesame.com.tn>
2021-01-04 14:29:03 +0000

    print integer

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,6 +1,7 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-        return True
     except (ValueError, TypeError):
         return False
+    else:
+        return True
