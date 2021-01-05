a8dd1dfa35d09286c72bb5dc4ef25b15f4a149c3
eya-98 <eya.nani@sesame.com.tn>
2021-01-05 15:41:20 +0000

    square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""class defines square"""
+
+
+class Square:
+    """square"""
+    def __init__(self, size):
+        """private instance attribute"""
+        self.__size = size 
