22012d99897807c5f2c8c971cc4ddf9d2cae657b
eya-98 <eya.nani@sesame.com.tn>
2021-01-05 16:28:57 +0000

    square

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -10,5 +10,5 @@ class Square:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        else: 
+        else:
             self.__size = size
