5244633b4eb48ddf12cb2f78f6bc6ef25c44494b
eya-98 <eya.nani@sesame.com.tn>
2021-01-05 16:27:39 +0000

    square

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,11 +6,9 @@ class Square:
     """square"""
     def __init__(self, size=0):
         """private instance attribute"""
-        try:
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else: 
             self.__size = size
-            if size < 0:
-                raise Exception
-        except TypeError:
-            print("size must be an integer")
-        except (ValueError, Exception):
-            print("size must be >= 0")
