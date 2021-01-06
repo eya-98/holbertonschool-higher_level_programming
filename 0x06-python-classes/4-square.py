0919ee3acc1b6e73d48fba4763d2a3a78bb1ce6c
eya-98 <eya.nani@sesame.com.tn>
2021-01-06 14:08:46 +0000

    square

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""class defines square"""
+
+
+class Square:
+    """square"""
+    def __init__(self, size=0):
+        """private instance attribute"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self._size = size
+
+    @property
+    def size(self):
+        """define size as a property"""
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        """sit the value of size"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self._size = value
+
+    def area(self):
+        """returns the square"""
+        a = self._size * self._size
+        return a
