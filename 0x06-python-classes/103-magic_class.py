b7b035a13ebab9656d56c843c83e680a37ed3c46
eya-98 <eya.nani@sesame.com.tn>
2021-01-07 19:35:25 +0000

    magic class

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -2,12 +2,13 @@
 """create a module"""
 
 
-class _MagicClass__radius:
+class MagicClass:
     """create a class"""
     from math import pi
     
     def __init__(self, radius=0):
         """initiation of radius"""
+        self.__radius = 0
         if type(radius) is not int or type(radius) is not float:
             raise TypeError("radius must be a number")
         else:
