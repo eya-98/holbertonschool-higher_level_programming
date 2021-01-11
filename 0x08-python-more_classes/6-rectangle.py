f55865596447cda16920c59addf67757fe80f41f
eya-98 <eya.nani@sesame.com.tn>
2021-01-11 19:57:10 +0000

    rectangle

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -4,9 +4,9 @@
 
 class Rectangle:
     """define rectangle class"""
-    
+
     number_of_instances = 0
-    
+
     def __init__(self, width=0, height=0):
         """instantation"""
         self.__width = width
