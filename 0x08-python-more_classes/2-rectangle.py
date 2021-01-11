e26e49af11002a9c97439ccc0d36571932821f54
eya-98 <eya.nani@sesame.com.tn>
2021-01-11 17:42:18 +0000

    rectangle

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -7,13 +7,13 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """instantiation of width and height)"""
-        self._width = width
-        self._height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
         """retrieve width"""
-        return self._width
+        return self.__width
 
     @width.setter
     def width(self, value):
@@ -23,12 +23,12 @@ class Rectangle:
         elif value < 0:
             raise ValueError("width must be >= 0")
         else:
-            self._width = value
+            self.__width = value
 
     @property
     def height(self):
         """retrieve width"""
-        return self._height
+        return self.__height
 
     @height.setter
     def height(self, value):
@@ -38,14 +38,14 @@ class Rectangle:
         elif value < 0:
             raise ValueError("height must be >= 0")
         else:
-            self._height = value
+            self.__height = value
 
     def area(self):
         """define area"""
-        return self._width * self._height
+        return self.__width * self.__height
 
     def perimeter(self):
         """define the perimeter"""
-        if width == 0 or height == 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
-        return (self._height + self._width) * 2
+        return (self.__height + self.__width) * 2
