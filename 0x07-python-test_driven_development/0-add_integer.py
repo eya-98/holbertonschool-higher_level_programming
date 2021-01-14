1ed1593538ce3b2753095197036aeb463192e799
eya-98 <eya.nani@sesame.com.tn>
2021-01-14 22:06:18 +0000

    add integer function

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
+"""
+
+define the module for holberton
+
+"""
 def add_integer(a, b=98):
+    """
+    define a function that add two integers
+    """
     if type(a) is not int:
         if type(a) is not float:
             raise TypeError("a must be an integer")
         else:
             a = int(a)
-    elif type(b) is not int:
+    if type(b) is not int:
         if type(b) is not float:
             raise TypeError("b must be an integer")
         else:
             b = int(b)
-    return a+b
+    return a + b
