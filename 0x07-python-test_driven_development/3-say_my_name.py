9bd0a779465d92c00e47659e3ae2166a7277917e
eya-98 <eya.nani@sesame.com.tn>
2021-01-16 01:32:04 +0000

    print my first and last name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,18 @@
+#!usr/bin/python3
+"""
+
+Holberton test module
+
+
+"""
+def say_my_name(first_name, last_name=""):
+   """
+   print My name is <first name> <last name>
+   
+   """
+   if type(first_name) is not str:
+       raise TypeError("first_name must be a string")
+   elif type(last_name) is not str:
+       raise TypeError("last_name must be a string")
+   else:
+       print("My name is {:s} {:s}".format(first_name, last_name))
