15ded5f579733a3395c92d489e5b384c131a75fa
eya-98 <eya.nani@sesame.com.tn>
2021-01-16 02:00:34 +0000

    print square test

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+
+Holberton test module
+
+"""
+
+
+def print_square(size):
+    """
+    
+    prints a square with the character #
+    """
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    elif size < 0:
+        raise ValueError("size must be >= 0")
+    else:
+        for i in range(size):
+            for j in range(size):
+                print('#', end="")
+            print("")
+                
+        
