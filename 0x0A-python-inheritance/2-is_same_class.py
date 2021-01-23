f1821a0f725d505e430588f66567292b2c576cdc
eya-98 <eya.nani@sesame.com.tn>
2021-01-23 02:11:36 +0000

    is same class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!usr/bin/python3
+"""holberton Module"""
+
+
+def is_same_class(obj, a_class):
+    """check if the object is exactly an instance of that class"""
+    return (type(obj) == a_class)
