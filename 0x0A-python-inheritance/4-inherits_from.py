9bd35300d3da258e3ac449ccc941d6d70c17e89b
eya-98 <eya.nani@sesame.com.tn>
2021-01-25 14:53:24 +0000

    inheritance

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Holberton Module"""
+
+
+def inherits_from(obj, a_class):
+    """check if the object is an instance of a class\
+    that inherited (directly or indirectly) from the\
+    specified class"""
+    return isinstance(obj, a_class)
+    
