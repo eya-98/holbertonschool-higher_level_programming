abf6e0db5474ad3cc3e1cac053152a441446bc25
eya-98 <eya.nani@sesame.com.tn>
2021-01-25 17:19:52 +0000

    base geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Holberton Module"""
+
+
+class BaseGeometry:
+    """Class named BaseGeometry"""
+    
+    def area(self):
+        """raise an exception"""
+        raise Exception("area() is not implemented")
