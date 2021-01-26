e8ff6bf521922f856f96d1c908c29132d9e51805
eya-98 <eya.nani@sesame.com.tn>
2021-01-26 10:49:16 +0000

    square

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Holberton Module"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """define a subclass class Square"""
+    def __init__(self, size):
+        if self.integer_validator("size", size):
+            self.__size = size
+        super().__init__(size, size)
+
+
