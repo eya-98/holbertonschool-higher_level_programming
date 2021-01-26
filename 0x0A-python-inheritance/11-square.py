ef5b52a76e0cfc1cdf462fa7fce575a6abf092c9
eya-98 <eya.nani@sesame.com.tn>
2021-01-26 11:50:02 +0000

    square

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
-"""Holberton Module"""
-Rectangle = __import__("9-rectangle").Rectangle
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """define a subclass class Square"""
+    """
+       Square that inherits from Rectangle (9-rectangle.py).
+       (task based on 10-square.py).
+    """
+
     def __init__(self, size):
-        if self.integer_validator("size", size):
-            self.__size = size
-        super().__init__(size, size)
-    
+        self.integer_validator("size", size)
+        Rectangle.__init__(self, size, size)
+        self.__size = size
+
     def __str__(self):
-        """print square details"""
+        """
+           return a string with "[Square] size/size"
+        """
         return "[Square] {}/{}".format(self.__size, self.__size)
