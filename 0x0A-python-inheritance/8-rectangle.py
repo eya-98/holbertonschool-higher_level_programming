262fb34b86da37a27e91b854740c20f32e1301ef
eya-98 <eya.nani@sesame.com.tn>
2021-01-26 09:17:44 +0000

    rectangle

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,9 +2,10 @@
 """Holberton Module"""
 BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     """class rectangle"""
-    
+
     def __init__(self, width, height):
         """instantiation of width and height"""
         self.integer_validator("width", width)
