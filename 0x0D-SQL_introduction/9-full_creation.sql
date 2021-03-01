485f8d483447a1c39e8496777bc726093bc9f512
eya-98 <eya.nani@sesame.com.tn>
2021-03-01 17:52:53 +0000

    full creation

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -2,7 +2,7 @@
 CREATE IF NOT EXISTS table second_table (id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table(id, name, score)
 VALUES
-("1", "John", "10",
-("2", "Alex", "3"),
-("3", "Bob", "14"),
-("4", "George", "8");
+(1, "John", 10),
+(2, "Alex", 3),
+(3, "Bob", 14),
+(4, "George", 8);
