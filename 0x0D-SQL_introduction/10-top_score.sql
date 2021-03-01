cffa05fda69257d2d01bf1bb5e7f937450ddaa58
eya-98 <eya.nani@sesame.com.tn>
2021-03-01 17:56:34 +0000

    sorting scores

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,3 @@
--- lists all records of the table second_table
-select score, name FROM second_table ORDER BY score DESC;
+-- lists all records of the table 
+
+SELECT score, name FROM second_table ORDER BY score DESC;
