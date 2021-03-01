49b60a0630af16e0f55a7f09b2e883e1d527073d
eya-98 <eya.nani@sesame.com.tn>
2021-03-01 20:02:37 +0000

    link

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,3 @@
 -- lists all records of the table second_table
 
-SELECT score, name FROM second_table WHERE name exists ORDER BY score DESC; 
+SELECT score, name FROM second_table WHERE name is not null ORDER BY score DESC; 
