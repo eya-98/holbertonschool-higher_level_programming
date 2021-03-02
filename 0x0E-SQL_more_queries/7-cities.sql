5a49ad9319bac511c44769533e375a2549fe6411
eya-98 <eya.nani@sesame.com.tn>
2021-03-02 19:15:07 +0000

    creta database and a table

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,7 +1,6 @@
 -- creates the database hbtn_0d_usa and the table cities.
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities(id INT AUTO_INCREMENT NOT NULL PRIMARY KEY, 
+CREATE TABLE IF NOT EXISTS cities(id INT AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY, 
     state_id INT NOT NULL FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id),
     name VARCHAR(256) NOT NULL);
