48973211f2a90915b19d8bf16282985c565ab679
eya-98 <eya.nani@sesame.com.tn>
2021-03-02 18:49:31 +0000

    create table

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,4 @@
+--   creates the database hbtn_0d_usa and the table states
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(id INT AUTO_INCREMENT NOT null PRIMARY KEY, name VARCHAR(256) NOT NULL );
