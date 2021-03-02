dcd17ccbfb8f2efbc22ffaed6b44cb7be18f8cc8
eya-98 <eya.nani@sesame.com.tn>
2021-03-02 16:15:14 +0000

    create

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- creates the database hbtn_0d_2 and the user user_0d_2
 
-CREATE IF NOT EXISTS hbtn_0d_2;
-CREATE IF NOT EXISTS 'user_0d_2'@'localhost';
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
 GRANT SELECT ON *.* TO 'user_0d_2'@'localhost';
