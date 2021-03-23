455c69ca832b2e5959d3f3f78bb617267d1468cf
eya-98 <eya.nani@sesame.com.tn>
2021-03-23 11:54:55 +0000

    print arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 let var1 = 0;
 process.argv.forEach((val, index) => {
-    var1++;
+  var1++;
 });
 if (var1 < 3) {
-    console.log("No argument");
+  console.log('No argument');
 } else if (var1 === 3) {
-    console.log("Argument found");   
+  console.log('Argument found');
 } else {
-    console.log("Arguments found");   
+  console.log('Arguments found');
 }
