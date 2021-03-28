db0a0cf6bf2b08253924dcc38eed34ce02ddbb61
eya-98 <eya.nani@sesame.com.tn>
2021-03-28 16:13:23 +0000

    select states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"
+lists  all states from database hbtn_0e_0_usa
+"
+import MySQLdb
+import sys
+var = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+                        passwd=sys.argv[2],
+                        db=sys.argv[3],
+                        charset="utf8")
+cursor = var.cursor()
+query = "SELECT id,name FROM states ORDER by states.id ASC"
+cursor.Execute(query)
+var.commit()
+cursor.close()
+var.close()
