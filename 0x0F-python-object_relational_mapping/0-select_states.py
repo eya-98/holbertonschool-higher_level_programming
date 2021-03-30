dfc18578406c4a6e4570f74f2d2701d101853330
eya-98 <eya.nani@sesame.com.tn>
2021-03-30 12:43:30 +0000

    select states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,24 +4,16 @@ lists  all states from database hbtn_0e_0_usa
 """
 import MySQLdb
 import sys
-
-
-def print_state():
-    """print states"""
-    var = MySQLdb.connect(host='localhost',
-                          port=3306,
-                          user=sys.argv[1],
-                          passwd=sys.argv[2],
-                          db=sys.argv[3])
-    cursor = var.cursor()
-    query = "SELECT id,name FROM states ORDER by states.id ASC"
-    cursor.execute(query)
-    Rows = cursor.fetchall()
-    for row in Rows:
-        print(row)
-    cursor.close()
-    var.close()
-
-
-if __name__ == "__main__":
-    print_state()
+var = MySQLdb.connect(host='localhost',
+                      port=3306,
+                      user=sys.argv[1],
+                      passwd=sys.argv[2],
+                      db=sys.argv[3])
+cursor = var.cursor()
+query = "SELECT id,name FROM states ORDER by states.id ASC"
+cursor.execute(query)
+Rows = cursor.fetchall()
+for row in Rows:
+    print(row)
+cursor.close()
+var.close()
