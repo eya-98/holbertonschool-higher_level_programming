9c45744ae067473dbdc349fafcea1f97f010a72b
eya-98 <eya.nani@sesame.com.tn>
2021-03-30 12:44:00 +0000

    select states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,6 +4,8 @@ lists  all states from database hbtn_0e_0_usa
 """
 import MySQLdb
 import sys
+
+
 var = MySQLdb.connect(host='localhost',
                       port=3306,
                       user=sys.argv[1],
