5134c8b5c2b490835d5839642c236611ab118633
eya-98 <eya.nani@sesame.com.tn>
2021-03-30 20:10:49 +0000

    filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,7 +7,7 @@ import sys
 def lists_states():
     var = MySQLdb.connect()
     cursor = var.cursor()
-    Query = "SELECT * FROM STATES WHERE states.name like "N%"
+    Query = "SELECT * FROM STATES WHERE states.name like 'N%'"
     cursor.execute(query)
     rows = var.fetchall()
     for row in rows:
