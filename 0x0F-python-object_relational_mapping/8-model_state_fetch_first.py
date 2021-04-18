da77841ae63514f1cac2209ff514eb0f4fed9254
eya-98 <eya.nani@sesame.com.tn>
2021-04-18 13:00:50 +0000

    list first state

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+prints the first State object from the database hbtn_0e_6_usa
+"""
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+if __name == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(engine)
+    Ses = sessionmaker(bind=eng)
+    session = Ses()
+    first = session.query(State).order_by(State.id).first()
+    if first is None:
+        print("Nothing")
+    else:
+        print("{}: {}".format(first_state.id, first_state.name))
+    session.close()
