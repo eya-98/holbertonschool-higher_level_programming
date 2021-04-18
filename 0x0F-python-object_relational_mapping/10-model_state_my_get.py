504ba11d88807734bf28baf8ef8ccc647fa41e5e
eya-98 <eya.nani@sesame.com.tn>
2021-04-18 20:29:10 +0000

    filter by name

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+prints the State object with the name passed as argument from the database
+"""
+
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+from model_state import Base, State
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                                                                    argv[2],
+                                                                    argv[3]))
+    Base.metadata.create_all(engine)
+    Ses = sessionmaker(bind=eng)
+    session = Ses()
+    state = session.query(State).filter_by(name=argv[4]).first()
+    if state is None:
+        print("Not found")
+    else:
+        print(State.id)
+    session.close()
