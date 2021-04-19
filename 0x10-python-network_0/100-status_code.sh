a1f93bb1298f41e1b8b6ba79299165f931f897c1
eya-98 <eya.nani@sesame.com.tn>
2021-04-19 16:28:14 +0000

    status code

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #sends a request to a URL passed as an argument, and displays only the status code of the response.
-curl -sow /dev/null "%{http_code}" "$1"
+curl -o /dev/null -s -w "%{http_code}" "$1"
