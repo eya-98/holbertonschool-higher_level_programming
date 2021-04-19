f41c319f78764edadef2317cb3e4b47c8433dd14
eya-98 <eya.nani@sesame.com.tn>
2021-04-19 16:07:28 +0000

    peak of a list

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -10,4 +10,5 @@ def find_peak(list_of_integers):
         for j in range(1, len(list_of_integers) - 1):
             if (list_of_integers[j] >= list_of_integers[j - 1] and list_of_integers[j] >= list_of_integers[j+1]):
                 i = list_of_integers[j]
+                j += 1
         return i
