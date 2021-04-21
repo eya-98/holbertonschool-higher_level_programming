4d060f7bc3bb518cb72536ba8d98325620cd17c6
eya-98 <eya.nani@sesame.com.tn>
2021-04-21 15:01:44 +0000

    post mail

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -3,11 +3,11 @@
 sends a POST request to the passed URL with the email
 as a parameter, and displays the body of the response
 """
+import sys
+import urllib.request
+import urllib.parse
 
 if __name__ == "__main__":
-    import sys
-    import urllib.request
-    import urllib.parse
     url = sys.argv[1]
     value = {'email': sys.argv[2]}
     mail = urllib.parse.urlencode(value).encode('utf-8')
