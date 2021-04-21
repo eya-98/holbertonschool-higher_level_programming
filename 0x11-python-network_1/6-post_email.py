3af7524e5112247cee3c5343763518d3b08bd4f2
eya-98 <eya.nani@sesame.com.tn>
2021-04-21 16:11:28 +0000

    post mail

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+sends a POST request to the passed URL with the email
+as a parameter, and displays the body of the response
+"""
+import sys
+import requests
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {'email': sys.argv[2]}
+    body = requests.post(url, value)
+    r = requests.get(url)
+    print(r.text)
