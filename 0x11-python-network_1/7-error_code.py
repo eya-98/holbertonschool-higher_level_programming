7f58b4ea61c73506e98905503da8ef47692eae47
eya-98 <eya.nani@sesame.com.tn>
2021-04-21 16:23:48 +0000

    error exception

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""sends a request to the URL and displays the body of the response"""
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    request = requests.get(url)
+    if request.status_code > 400:
+        print ('Error code: {}'.format(request.code_status))
+    else:
+        print(request.text)
