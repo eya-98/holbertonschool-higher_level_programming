1caa7f9d987cbfe0c185c2259c232bbef35def8f
eya-98 <eya.nani@sesame.com.tn>
2021-04-21 16:28:24 +0000

    post request

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""
-sends a POST request to the passed URL with the email
-as a parameter, and displays the body of the response
-"""
+"""sends a POST request"""
 import sys
 import urllib.request
 import urllib.parse
