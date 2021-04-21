422df3a7911abe45b03ef0923abe723eb0b24d11
eya-98 <eya.nani@sesame.com.tn>
2021-04-21 15:15:43 +0000

    exception error

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""sends a request to the URL and displays the body of the response"""
+import sys
+import urllib.request
+from urllib.error import HTTPError, URLError
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    try:
+        request = urllib.request.Request(url)
+        with urllib.request.urlopen(request) as response:
+            html = response.read()
+            print (html)
+    except urllib.error.HTTPError as error:
+        print ('Error code: {}'.format(error.code))
