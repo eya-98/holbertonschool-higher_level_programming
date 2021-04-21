85bcd5061b9c9335ee8523346f8207baa87089e5
eya-98 <eya.nani@sesame.com.tn>
2021-04-21 16:15:00 +0000

    post mail

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -8,6 +8,5 @@ import requests
 if __name__ == "__main__":
     url = sys.argv[1]
     value = {'email': sys.argv[2]}
-    body = requests.post(url, value)
-    r = requests.get(url)
-    print(r.text)
+    body = requests.post(url, value) 
+    print(body.text)
