6420fcf59de3c1573dd4bc7b037ae257fddc59d2
eya-98 <eya.nani@sesame.com.tn>
2021-04-21 13:15:16 +0000

    fetches URL

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" fetches https://intranet.hbtn.io/status"""
+import urllib.request
+
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(html.__class__))
+    print("\t- content: {}".format(html))
+    print("\t- utf8 content: {}".format(html.decode('utf8')))
