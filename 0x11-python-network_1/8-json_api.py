a8e004cdedb384d64d7af6258b1d866846cb1c6a
eya-98 <eya.nani@sesame.com.tn>
2021-04-21 16:37:50 +0000

    json typ

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user 
+with the letter as a parameter.
+"""
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = 'http://0.0.0.0:5000/search_user'
+    try:
+        q = sys.argv[1]
+    except Exception:
+        q= ""
+    body = requests.post(url, data={'q': q})
+    request = body.json()
+    try:
+        if request:
+            print("[{}] {}".format(result.get('id'), result.get('name')))
+        else:
+            print('No result')
+    except Exception:
+        print('Not a valid JSON')
