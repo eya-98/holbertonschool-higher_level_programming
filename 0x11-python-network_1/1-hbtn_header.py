e69ebed0ac9b41bb6220b0b1351d93e0cf42943c
eya-98 <eya.nani@sesame.com.tn>
2021-04-21 13:47:54 +0000

    X-Request-Id

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -9,5 +9,4 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     with urllib.request.urlopen(url) as response:
-        html = response.read()
         print (response.getheader('X-Request-Id'))
