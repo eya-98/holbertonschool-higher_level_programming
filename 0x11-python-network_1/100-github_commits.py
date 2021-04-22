a8a8c75cbcd823520d431ffc8057434b129fbd73
eya-98 <eya.nani@sesame.com.tn>
2021-04-22 14:57:22 +0000

    githup API for commits

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,10 +7,10 @@ from sys import argv
 if __name__ == "__main__":
     repo = argv[1]
     owner = argv[2]
-    url = "https://api.github.com/repos" + repo + "/" + owner + "/commits"
+    url = "https://api.github.com/repos" + owner + "/" + repo + "/commits"
     reqs = requests.get(url)
     requets = reqs.json()[:10]
-    for req in requests:
+    for req in requets:
         sha = req['sha']
         commits = req['commit']['author']['name']
         print("{}: {}".format(sha, commits))
