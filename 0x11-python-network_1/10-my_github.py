9ca51ac57d71611cbf3fb175d582a49c5089f608
eya-98 <eya.nani@sesame.com.tn>
2021-04-22 14:15:08 +0000

    github credentials

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+takes your GitHub credentials (username and password) and 
+uses the GitHub API to display your id
+"""
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    username = argv[1]
+    passwd = argv[2]
+    req = requests.get('' , auth=(username, passwd))
+    if req.status_code > 400:
+        print('None')
+    else:
+         print(r.json()['id'])
