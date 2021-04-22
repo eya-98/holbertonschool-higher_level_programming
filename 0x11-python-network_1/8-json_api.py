87e16441e4c86ae0b9fc4679659dd16d3aa94321
eya-98 <eya.nani@sesame.com.tn>
2021-04-22 14:02:48 +0000

    deal with json

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user 
+takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user
 with the letter as a parameter.
 """
 import sys
