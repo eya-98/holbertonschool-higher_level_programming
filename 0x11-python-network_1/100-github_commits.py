33e8c01afe2d669c2ad145b01712b370eea73806
eya-98 <eya.nani@sesame.com.tn>
2021-04-22 14:52:13 +0000

    githup API for commits

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""list 10 commits (from the most recent to oldest)
+of the repository “rails” by the user “rails”"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    repo = argv[1]
+    owner = argv[2]
+    url = "https://docs.github.com/repos" + owner + "/" + repo + "/commits"
+    reqs = requests.get(url)
+    requets = reqs.json()[:10]
+    for req in requests:
+        sha = req['sha']
+        commits = req['commit']['author']['name']
+        print("{}: {}".format(sha, commits))
