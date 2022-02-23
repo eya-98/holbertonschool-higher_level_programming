09d009d61c7d16a314095d5fe36ec5cdb3665637
eya-98 <eya.nani@sesame.com.tn>
2022-02-23 09:26:41 +0100

    convert to int

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 const process = require('process');
-argv = process.argv
+const argv = process.argv;
 if (argv.length === 3) {
-    if (isNaN(Number(argv[2])) == false) {
-        console.log("My number: " + argv[2])
-     }
-    else {
-         console.log('Not a number')
-     }
- }
+  if (isNaN(Number(argv[2])) === false) {
+    console.log('My number: ' + argv[2]);
+  } else {
+    console.log('Not a number');
+  }
+}
