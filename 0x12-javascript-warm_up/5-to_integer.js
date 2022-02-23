adb2fd65d20f2cd7c837412b019ca44dad199b79
eya-98 <eya.nani@sesame.com.tn>
2022-02-23 09:20:53 +0100

    convert to int

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 const process = require('process');
 argv = process.argv
-console.log(argv[2])
 if (argv.length === 3) {
-    console.log(argv[2])
     if (isNaN(Number(argv[2])) == false) {
         console.log(argv[2] + ": <first argument converted in integer>")
      }
