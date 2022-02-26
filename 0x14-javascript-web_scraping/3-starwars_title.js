a472818db75d2e8ca4ef5d537a24b1e153060022
eya-98 <eya.nani@sesame.com.tn>
2022-02-26 14:23:23 +0100

    title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,7 +2,6 @@
 const process = require('process');
 const url = 'https://swapi-api.hbtn.io/api/films/:id';
 const id = process.argv[2];
-console.log(id);
 const theUrl = url.replace(':id', id);
 const request = require('request');
 request.get(theUrl, function (err, response) {
